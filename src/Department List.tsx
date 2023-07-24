98fe635d0d695d5067b4a83f16e6fdae1ffe3797
WINDOWS-0401\jaswa <jaswa0401@gmail.com>
2023-07-24 08:45:02 +0530

    third commit

--- src/Department List.tsx
+++ src/Department List.tsx
@@ -1,81 +1,91 @@
 // DepartmentTable.tsx
 import React, { useState } from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableRow, Checkbox } from '@mui/material';
-import { Department } from './models';
+import { Button, Collapse, List, ListItem, ListItemIcon, ListItemText, Checkbox } from '@mui/material';
+import { Department, SubDepartment } from './models';
 
-interface DepartmentListProps {
+interface DepartmentTableProps {
   data: Department[];
 }
 
-const DepartmentList: React.FC<DepartmentListProps> = ({ data }) => {
+const DepartmentTable: React.FC<DepartmentTableProps> = ({ data }) => {
   const [departments, setDepartments] = useState<Department[]>(data);
 
-  const handleDepartmentChange = (departmentId: number, checked: boolean) => {
-    const updatedDepartments = departments.map((dept) => {
-      if (dept.id === departmentId) {
-        dept.selected = checked;
-        dept.subDepartments.forEach((subDept) => (subDept.selected = checked));
-      } else {
-        const allSubSelected = dept.subDepartments.every((subDept) => subDept.selected);
-        if (allSubSelected) dept.selected = true;
-        else dept.selected = false;
-      }
-      return dept;
-    });
-
-    setDepartments(updatedDepartments);
+  const handleDepartmentClick = (departmentId: number) => {
+    const updatedData = departments.map((department) =>
+      department.id === departmentId
+        ? { ...department, selected: !department.selected, subDepartments: toggleSubDepartments(department.subDepartments, !department.selected) }
+        : department
+    );
+    setDepartments(updatedData);
   };
 
-  const handleSubDepartmentChange = (departmentId: number, subDepartmentId: number, checked: boolean) => {
-    const updatedDepartments = departments.map((dept) => {
-      if (dept.id === departmentId) {
-        const subDept = dept.subDepartments.find((sub) => sub.id === subDepartmentId);
-        if (subDept) {
-          subDept.selected = checked;
-          const allSubSelected = dept.subDepartments.every((sub) => sub.selected);
-          dept.selected = allSubSelected;
-        }
-      }
-      return dept;
-    });
+  const toggleSubDepartments = (subDepartments: SubDepartment[], selected: boolean) => {
+    return subDepartments.map((subDepartment) => ({ ...subDepartment, selected }));
+  };
 
-    setDepartments(updatedDepartments);
+  const handleSubDepartmentClick = (departmentId: number, subDepartmentId: number) => {
+    const updatedData = departments.map((department) => {
+      if (department.id === departmentId) {
+        const updatedSubDepartments = department.subDepartments.map((subDepartment) =>
+          subDepartment.id === subDepartmentId ? { ...subDepartment, selected: !subDepartment.selected } : subDepartment
+        );
+        const isAllSubDepartmentsSelected = updatedSubDepartments.every((subDepartment) => subDepartment.selected);
+        return { ...department, subDepartments: updatedSubDepartments, selected: isAllSubDepartmentsSelected };
+      } else {
+        return department;
+      }
+    });
+    setDepartments(updatedData);
+  };
+
+  const handleDropDownClick = (departmentId: number) => {
+    const updatedData = departments.map((department) =>
+      department.id === departmentId ? { ...department, showSubDepartments: !department.showSubDepartments } : department
+    );
+    setDepartments(updatedData);
   };
 
   return (
-    <TableContainer>
-      <Table>
-        <TableBody>
-          {departments.map((dept) => (
-            <React.Fragment key={dept.id}>
-              <TableRow>
-                <TableCell>
-                  <Checkbox
-                    checked={dept.selected}
-                    onChange={(e) => handleDepartmentChange(dept.id, e.target.checked)}
-                  />
-                </TableCell>
-                <TableCell>{dept.name}</TableCell>
-                <TableCell />
-              </TableRow>
-              {dept.subDepartments.map((subDept) => (
-                <TableRow key={subDept.id}>
-                  <TableCell />
-                  <TableCell>{subDept.name}</TableCell>
-                  <TableCell>
+    <List component="nav" dense>
+      {departments.map((department) => (
+        <React.Fragment key={department.id}>
+          <ListItem>
+            <ListItemIcon>
+              <Checkbox
+                edge="start"
+                checked={department.selected}
+                tabIndex={-1}
+                disableRipple
+                onClick={() => handleDepartmentClick(department.id)}
+              />
+            </ListItemIcon>
+            <ListItemText primary={department.name} />
+            <Button onClick={() => handleDropDownClick(department.id)}>
+              {department.showSubDepartments ? 'Hide Sub-departments' : 'Show Sub-departments'}
+            </Button>
+          </ListItem>
+          <Collapse in={department.showSubDepartments} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              {department.subDepartments.map((subDepartment) => (
+                <ListItem key={subDepartment.id} style={{ paddingLeft: 32 }}>
+                  <ListItemIcon>
                     <Checkbox
-                      checked={subDept.selected}
-                      onChange={(e) => handleSubDepartmentChange(dept.id, subDept.id, e.target.checked)}
+                      edge="start"
+                      checked={subDepartment.selected}
+                      tabIndex={-1}
+                      disableRipple
+                      onClick={() => handleSubDepartmentClick(department.id, subDepartment.id)}
                     />
-                  </TableCell>
-                </TableRow>
+                  </ListItemIcon>
+                  <ListItemText primary={subDepartment.name} />
+                </ListItem>
               ))}
-            </React.Fragment>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+            </List>
+          </Collapse>
+        </React.Fragment>
+      ))}
+    </List>
   );
 };
 
-export default DepartmentList;
+export default DepartmentTable;
--- src/models.ts
+++ src/models.ts
@@ -1,14 +1,14 @@
 // models.ts
 export interface Department {
-    id: number;
-    name: string;
-    subDepartments: SubDepartment[];
-    selected: boolean;
-  }
-  
-  export interface SubDepartment {
-    id: number;
-    name: string;
-    selected: boolean;
-  }
-  
+  id: number;
+  name: string;
+  subDepartments: SubDepartment[];
+  selected: boolean;
+  showSubDepartments: boolean; // Add this property to indicate if sub-departments are shown or hidden
+}
+
+export interface SubDepartment {
+  id: number;
+  name: string;
+  selected: boolean;
+}
